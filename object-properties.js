5963bab33ac25657ae8a2aa61ff532a2b9a77e01
UberStash <expiredserver@gmail.com>
2020-06-23 21:15:50 +0000

    The rest of javascripting

---
+++ object-properties.js
@@ -0,0 +1,8 @@
+   
+  // In that file, define a variable named food like this:  
+   
+  const food = {  
+    types: 'only pizza'  
+  }  
+
+  console.log(food.types) 
---
+++ objects.js
@@ -0,0 +1,11 @@
+// Create a file named objects.js.  
+   
+//   In that file, define a variable named pizza like this:  
+   
+     const pizza = {  
+       toppings: ['cheese', 'sauce', 'pepperoni'],  
+       crust: 'deep dish',  
+       serves: 2  
+     }  
+   
+      console.log(pizza)
